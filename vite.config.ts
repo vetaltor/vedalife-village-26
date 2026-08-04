cb282c5ce4862ef4e72a240aba31a56cacd0cf3d
vetaltor <vetaltor@gmail.com>
2026-08-04 18:01:52 +0300

    fixed config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import path from 'node:path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'node:path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/vedalife-village-26/', // GitHub repository name
-  resolve: {
-    alias: {
-      '@': path.resolve(import.meta.dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL, // GitHub repository name
+    resolve: {
+      alias: {
+        '@': path.resolve(process.cwd(), './src'),
+      },
     },
-  },
-})
+    server: {
+      port: 3000,
+    },
+  };
+});
