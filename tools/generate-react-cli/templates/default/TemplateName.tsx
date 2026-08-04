9f828c42d7ef87d94cd1e15d6a593c6c94586b28
vetaltor <vetaltor@gmail.com>
2026-08-04 18:47:19 +0300

    react grc

---
+++ tools/generate-react-cli/templates/default/TemplateName.tsx
@@ -0,0 +1,7 @@
+import s from './TemplateName.module.css';
+
+interface TemplateNameProps {}
+
+export function TemplateName(props: TemplateNameProps) {
+  return <div className={s.root}>TemplateName</div>;
+}
